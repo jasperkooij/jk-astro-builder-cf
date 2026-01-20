b3cc9149d93d41dd611c342efde492e6726f38ec
Jasper Kooij <jasperkooij@gmail.com>
2026-01-20 10:37:02 -0600

    Switch to Astro Tailwind integration for proper CSS bundling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
 import cloudflare from '@astrojs/cloudflare';
 
 import svelte from '@astrojs/svelte';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
     ssr: {
       external: ['sharp']
     }
   },
 
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/noop'
-    }
-  },
-
   adapter: cloudflare(),
-  integrations: [svelte()]
+  integrations: [svelte(), tailwind({ applyBaseStyles: false })]
 });
